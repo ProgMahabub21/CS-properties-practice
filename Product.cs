122c98956cfbe25562886b055943d75b68997b39
ProgMahabub21 <85748757+ProgMahabub21@users.noreply.github.com>
2021-06-20 20:42:56 +0600

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductInfo
+{
+    struct ManuDate
+    {
+        private byte day;
+        private byte month;
+        private short year;
+
+        public ManuDate(byte day, byte month, short year)
+        {
+
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+        public void PrintDate()
+        {
+            Console.WriteLine("Manufracture Date {0}/{1}/{2}", day, month, year);
+        }
+    }
+    class Product
+    {
+        private string id;
+        private float price;
+        private ManuDate date;
+        public Product()
+        {
+            //default constructor
+        }
+        public string ID            //property
+        {
+            get { return this.id; }     //get method
+            set { this.id = value; }  //set method 
+
+        }
+        public float Price
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+        public ManuDate Date
+        {
+            get { return this.date; }
+            set { this.date = value; }
+        }
+
+        public Product(string id, float price, ManuDate date)
+        {
+            this.ID = id;
+            this.Date=date;
+            this.Price = price;
+        }
+        public void PrintProduct()
+        {
+            Console.WriteLine("Product Information:-");
+            Console.WriteLine("ID {0}", this.ID);
+            this.date.PrintDate();
+            Console.WriteLine("Price  {0}", this.Price);
+        }
+
+    }
+}
